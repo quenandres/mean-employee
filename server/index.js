b9f07e1f6df9a1cefa7fe7fd6ac334d3cbc602a3
Jhonatan Mejia <sistemas@mejoraysoluciones.com>
2019-04-03 18:32:38 -0500

    Agrega archivos de server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express =require ('express');
-
+const morgan = require ('morgan');
 const app = express();
+
+const { mongoose } = require('./database');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
 
 // Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
 
 
 // Routes
