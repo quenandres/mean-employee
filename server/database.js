d8968b5127029bb7e1f336f6728dfbdd703302e9
quenandres <quenandres@gmail.com>
2019-04-04 10:54:11 -0500

    Asigna URI  de mlab

--- server/database.js
+++ server/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/mean-crud';
+//const URI = 'mongodb://localhost/mean-crud';
+const URI = 'mongodb://quenan:quenan2019@ds117858.mlab.com:17858/mean-employees-quenan-database';
 
 mongoose.connect(URI)
     .then(db => console.log("DB is connected !"))
