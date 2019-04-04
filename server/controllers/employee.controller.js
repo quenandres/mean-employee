a0cf509babfbbe1c138d138e4b90b2cdf64aaecf
Jhonatan Mejia <sistemas@mejoraysoluciones.com>
2019-04-03 23:34:41 -0500

    Termina planteamiento del API en express y mongo.

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -1,31 +1,44 @@
+const Employee = require('../models/employees');
+
 const employeeCtrl = {};
 
-employeeCtrl.getEmployees = (req,res)=>{
+employeeCtrl.getEmployees = async (req,res)=>{
+    const employees = await Employee.find();//El await dice que se podria demorar el proceso
+    res.json(employees);
+};
+
+employeeCtrl.createEmployee = async (req,res)=>{
+    const employee = new Employee(req.body);
+    await employee.save();
     res.json({
-        status:"Employees goes here !"
+        "status":"Guardado"
+    })
+};
+
+employeeCtrl.getEmployee = async (req,res)=>{
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee);
+}
+
+employeeCtrl.editEmployee = async (req,res)=>{
+    const { id } = req.params;
+    const employee = {
+        name:req.body.name,
+        position:req.body.position,
+        office:req.body.office,
+        salary:req.body.salary
+    };
+    await Employee.findByIdAndUpdate(id,{$set:employee},{new:true});
+
+    res.json({
+        "status":"Employee Update"
     });
 }
 
-employeeCtrl.createEmployee = (req,res)=>{
-    res.json({
-        status:"Create user"
-    });
-}
+employeeCtrl.deleteEmployee = async (req,res)=>{
+    await Employee.findByIdAndRemove(req.params.id);
+    res.json({"status":"Employee delete"});
 
-employeeCtrl.getEmployee = (req,res)=>{
-    res.json({
-        status:"Employee !"
-    });
-}
-
-employeeCtrl.editEmployee = (req,res)=>{
-    res.json({
-        status:"edit Employee !"
-    });
-}
-
-employeeCtrl.deleteEmployee = function () {
-    
 }
 
 
